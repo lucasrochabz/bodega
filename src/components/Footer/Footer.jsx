ff4167765c301b4494d8ac82994565441297b6f0
lucasrochabz <lucasbezerrar@gmail.com>
2024-10-10 20:30:25 -0300

    style: add estilo ao footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>Bodega &copy;. Todos os direitos reservados.</p>
+      <p>Bodega &copy; Todos os direitos reservados.</p>
     </div>
   );
 };
