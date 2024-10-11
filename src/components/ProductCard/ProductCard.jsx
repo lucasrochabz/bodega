76d5b2e67e3f9f40a4060c30124b8f8f7007a4e4
lucasrochabz <lucasbezerrar@gmail.com>
2024-10-11 15:09:27 -0300

    style: add imagens

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,15 @@
 import './ProductCard.css';
+// import imageTeste from '../../assets/images/image-teste.png';
+import note from '../../assets/images/notebook-2.jpg';
 
 export const ProductCard = ({ item }) => {
   return (
     <div className="product-card">
+      <img src={note} alt={item.name} />
       <p className="price">{item.price}</p>
-      <h2 key={item.id}>{item.name}</h2>
+      <h2 className="title" key={item.id}>
+        {item.name}
+      </h2>
       <p>{item.descricao}</p>
     </div>
   );
