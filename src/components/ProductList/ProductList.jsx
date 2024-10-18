92d6b609d676074aeb0e15471588409934283281
lucasrochabz <lucasbezerrar@gmail.com>
2024-10-18 14:52:48 -0300

    add logo

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,66 +5,66 @@ export const ProductList = () => {
   const product = [
     {
       id: 1,
-      name: 'Mesa',
+      name: 'Notebook',
       price: 'R$ 1.200,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Notebook é ideal para quem busca desempenho e portabilidade em um único aparelho.',
     },
     {
       id: 2,
-      name: 'Cadeira',
+      name: 'Smartphone',
       price: 'R$ 2.400,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Smartphone oferece tecnologia avançada e recursos inovadores para facilitar o seu dia a dia.',
     },
     {
       id: 3,
-      name: 'Banco',
+      name: 'Câmera',
       price: 'R$ 3.800,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'A Câmera proporciona imagens de alta qualidade, capturando momentos únicos com precisão incrível.',
     },
     {
       id: 4,
-      name: 'Mesa',
+      name: 'Smartwatch',
       price: 'R$ 1.200,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Smartwatch combina estilo e funcionalidade, monitorando sua saúde e conectando você ao mundo.',
     },
     {
       id: 5,
-      name: 'Cadeira',
+      name: 'Tablet',
       price: 'R$ 2.400,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Tablet é perfeito para entretenimento e trabalho, oferecendo versatilidade em um design compacto.',
     },
     {
       id: 6,
-      name: 'Banco',
+      name: 'Tablet',
       price: 'R$ 3.800,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Tablet é perfeito para entretenimento e trabalho, oferecendo versatilidade em um design compacto.',
     },
     {
       id: 7,
-      name: 'Mesa',
+      name: 'Câmera',
       price: 'R$ 1.200,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'A Câmera proporciona imagens de alta qualidade, capturando momentos únicos com precisão incrível.',
     },
     {
       id: 8,
-      name: 'Cadeira',
+      name: 'Smartphone',
       price: 'R$ 2.400,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Smartphone oferece tecnologia avançada e recursos inovadores para facilitar o seu dia a dia.',
     },
     {
       id: 9,
-      name: 'Banco',
+      name: 'Notebook',
       price: 'R$ 3.800,00',
       descricao:
-        'Gostaria de enfatizar que o início da atividade geral de formação de atitudes pode nos levar a considerar.',
+        'O Notebook é ideal para quem busca desempenho e portabilidade em um único aparelho.',
     },
   ];
 
