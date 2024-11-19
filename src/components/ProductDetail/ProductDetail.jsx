e3daa4d3894092c64fc99f9f1dce811cd57ecea0
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-19 16:01:51 -0300

    corrigindo funcao para calcular total

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -5,13 +5,27 @@ import notebook from '../../assets/images/notebook-2.jpg';
 import './ProductDetail.css';
 
 export const ProductDetail = () => {
-  const [product, setProduct] = useState({});
   const { productId } = useParams();
+
+  const [product, setProduct] = useState({});
+  const [stock, setStock] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const getProduct = async () => {
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const results = await response.json();
     setProduct(results.data);
+    setStock(results.data.stock);
+    setPrice(results.data.price);
+  };
+
+  const handleClick = () => {
+    console.log('Você clicou em comprar.');
+    setStock((prevStock) => prevStock - 1);
+    setQuantity((prevQuantity) => prevQuantity + 1);
+    setTotal(price * quantity);
   };
 
   useEffect(() => {
@@ -25,10 +39,10 @@ export const ProductDetail = () => {
         <h1>{product.name}</h1>
         <p className="info-prince">{product.price}</p>
         <p className="info-descricao">{product.description}</p>
-        <p>Estoque: {product.stock}</p>
-        {/* <p>Quantidade: {quantity}</p> */}
-        {/* <h2 className="info-total">Total: {formattedPrice(total)}</h2> */}
-        <ButtonBuy text={'Comprar'} />
+        <p>Estoque: {stock}</p>
+        <p>Quantidade: {quantity}</p>
+        <h2 className="info-total">Total: R$ {total.toFixed(2)}</h2>
+        <ButtonBuy handleClick={handleClick} text={'Adicionar'} />
       </div>
     </div>
   );
