a8f97b37ad34326a2b3ce050719d64d86489f7f6
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-03 15:48:21 -0300

    add bloco try catch ao productDetail.jsx

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -21,20 +21,31 @@ export const ProductDetail = () => {
   };
 
   const handleClick = async () => {
-    const response = await fetch('http://localhost:4000/orders', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        user_id: 1,
-        address_id: 1,
-        date: '2024-11-29',
-        status: 'Aguardando Pagamento',
-        product_id: product.id,
-      }),
-    });
-    const results = await response.json();
-    const ordersPath = `/orders/${results.data.id}`;
-    window.location.href = ordersPath;
+    try {
+      const response = await fetch('http://localhost:4000/orders', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          user_id: 1,
+          address_id: 1,
+          date: '2024-11-29',
+          status: 'Aguardando Pagamento',
+          product_id: product.id,
+        }),
+      });
+
+      if (!response.ok) {
+        const results = await response.json();
+        throw new Error(results.message);
+      }
+
+      const results = await response.json();
+      const ordersPath = `/orders/${results.data.id}`;
+      window.location.href = ordersPath;
+    } catch (error) {
+      console.error('Erro na requisição: ', error.message);
+      alert(`Erro ao fazer pedido: ${error.message}`);
+    }
   };
 
   useEffect(() => {
