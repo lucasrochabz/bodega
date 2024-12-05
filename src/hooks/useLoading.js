0ae7d2cfb92dd3b3408c0956769b489639040074
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-05 09:32:50 -0300

    add custom hook useLoading

---
+++ src/hooks/useLoading.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useLoading = () => {
+  const [loading, setLoading] = useState(false);
+
+  const startLoading = () => {
+    setLoading(true);
+  };
+
+  const stopLoading = () => {
+    setLoading(false);
+  };
+
+  return {
+    startLoading,
+    stopLoading,
+    loading,
+  };
+};
--- src/pages/OrderCompleted/OrderCompleted.jsx
+++ src/pages/OrderCompleted/OrderCompleted.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useLoading } from '../../hooks';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { OrderList } from '../../components/OrderList';
@@ -8,14 +9,22 @@ import './OrderCompleted.css';
 export const OrderCompleted = () => {
   const { orderId } = useParams();
 
-  const [loading, setLoading] = useState(true);
+  const { startLoading, stopLoading, loading } = useLoading();
+
   const [order, setOrder] = useState({});
 
   const getOrder = async () => {
-    const response = await fetch(`http://localhost:4000/orders/${orderId}`);
-    const results = await response.json();
-    setOrder(results.data);
-    setLoading(false);
+    startLoading();
+    try {
+      const response = await fetch(`http://localhost:4000/orders/${orderId}`);
+      const results = await response.json();
+      setOrder(results.data);
+      stopLoading();
+    } catch (error) {
+      console.error('Erro ao buscar pedido:' + error);
+    } finally {
+      stopLoading();
+    }
   };
 
   useEffect(() => {
