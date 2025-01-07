e9c981d0b7f25d595e6b18d6c22b6f2aca0e50b7
lucasrochabz <lucasbezerrar@gmail.com>
2025-01-07 10:47:51 -0300

    pequeno ajuste

--- src/components/SignUpModal/SignUpModal.jsx
+++ src/components/SignUpModal/SignUpModal.jsx
@@ -17,7 +17,6 @@ export const SignUpModal = ({ isModalOpen }) => {
   const getCep = async () => {
     if (cep.length === 8) {
       const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-      console.log(response);
       const cepResult = await response.json();
       if (cepResult.erro === 'true') {
         return alert('Cep inválido');
