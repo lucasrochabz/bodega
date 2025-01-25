0974d8cf8f6d42d36d74cd3a205223d5a459f603
lucasrochabz <lucasbezerrar@gmail.com>
2025-01-25 14:54:29 -0300

    add rota para navegar para detalhe do pedido

--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import { formattedDate } from '../../utils/dateUtils';
 import './OrdersList.css';
 
 export const OrdersList = ({ orders }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (orderId) => {
+    navigate(`/orders/details/${orderId}`);
+  };
+
   return (
     <>
       {orders.map((order) => (
-        <div className="order-list" key={order.id}>
+        <div
+          className="order-list"
+          key={order.id}
+          onClick={() => handleClick(order.id)}
+        >
           <p>{order.id}</p>
           <p>Data: {formattedDate(order.date)}</p>
           <p>{order.name}</p>
