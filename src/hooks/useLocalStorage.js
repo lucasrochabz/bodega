aab42948c4599057f5267a6ef3e473d6d903cf07
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-13 12:15:52 -0300

    removendo window

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 
 export const useLocalStorage = (storageKey, defaultValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    const localStorageValue = window.localStorage.getItem(storageKey);
+    const localStorageValue = localStorage.getItem(storageKey);
 
     return localStorageValue ? localStorageValue : defaultValue;
   });
 
   useEffect(() => {
-    window.localStorage.setItem(storageKey, storedValue);
+    localStorage.setItem(storageKey, storedValue);
   }, [storageKey, storedValue]);
 
   return [storedValue, setStoredValue];
