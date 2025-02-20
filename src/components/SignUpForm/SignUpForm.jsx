58b5e84d436052d823ad864f696fac1e0c23c8e2
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-20 11:06:50 -0300

    corrigindo fluxo do cadastro

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,27 +1,167 @@
-import { useState } from 'react';
-import { SignUpModal } from '../SignUpModal';
+import { useEffect, useState } from 'react';
+import { BASE_API_URL } from '../../../config';
+import { Input } from '../Input';
 import './SignUpForm.css';
 
 export const SignUpForm = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [zipCode, setZipCode] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [numero, setNumero] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
 
-  const toggleModal = () => {
-    setIsModalOpen((prevIsModalOpen) => !prevIsModalOpen);
+  const getCep = async () => {
+    if (zipCode.length === 8) {
+      const response = await fetch(`https://viacep.com.br/ws/${zipCode}/json`);
+      const cepResult = await response.json();
+      if (cepResult.erro) {
+        alert('CEP inválido');
+        return;
+      }
+      setEndereco(cepResult.logradouro);
+      setBairro(cepResult.bairro);
+      setCidade(cepResult.localidade);
+      setEstado(cepResult.uf);
+    } else {
+      setEndereco('');
+      setBairro('');
+      setCidade('');
+      setEstado('');
+    }
+  };
+
+  useEffect(() => {
+    getCep();
+  }, [zipCode]);
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`${BASE_API_URL}/users`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+          zip_code: zipCode,
+          street: endereco,
+          number: numero,
+          neighborhood: bairro,
+          city: cidade,
+          state: estado,
+        }),
+      });
+
+      if (!response.ok) {
+        const results = await response.json();
+        throw new Error(results.message);
+      }
+
+      const results = await response.json();
+      alert(results.message);
+      setName('');
+      setEmail('');
+      setPassword('');
+      setZipCode('');
+      setNumero('');
+    } catch (error) {
+      console.error('Erro na requisição:', error.message);
+      alert(`Erro ao cadastrar usuário: ${error.message}`);
+    }
   };
 
   return (
-    <div className="signup-form">
-      <h2 className="default-title">Crie a sua conta</h2>
-      {isModalOpen && (
-        <SignUpModal
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-        />
-      )}
+    <form className="signup" onSubmit={handleSignup}>
+      <Input
+        label="Name"
+        id="name"
+        value={name}
+        setValue={setName}
+        placeholder="Name completo"
+        required
+      />
 
-      <button className="btn-signup-form" onClick={toggleModal}>
-        {isModalOpen ? 'Já tem uma conta?' : 'Criar conta'}
-      </button>
-    </div>
+      <Input
+        type="email"
+        label="Email"
+        id="email"
+        value={email}
+        setValue={setEmail}
+        placeholder="exemplo@email.com"
+        required
+      />
+
+      <Input
+        type="password"
+        label="Senha"
+        id="password"
+        value={password}
+        setValue={setPassword}
+        required
+      />
+
+      <Input
+        type="number"
+        label={'CEP'}
+        id={'cep'}
+        value={zipCode}
+        setValue={setZipCode}
+        placeholder="60000000"
+        required
+      />
+
+      <Input
+        type="text"
+        label={'Endereço'}
+        id={'endereco'}
+        value={endereco}
+        readOnly
+        required
+      />
+
+      <Input
+        type="number"
+        label={'Número'}
+        id={'numero'}
+        value={numero}
+        setValue={setNumero}
+        required
+      />
+
+      <Input
+        type="text"
+        label={'Bairro'}
+        id={'bairro'}
+        value={bairro}
+        readOnly
+        required
+      />
+
+      <Input
+        type="text"
+        label={'Cidade'}
+        id={'cidade'}
+        value={cidade}
+        readOnly
+        required
+      />
+
+      <Input
+        type="text"
+        label={'Estado'}
+        id={'estado'}
+        value={estado}
+        readOnly
+        required
+      />
+
+      <button className="btn-signup">Criar</button>
+    </form>
   );
 };
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { Head } from '../../components/Head';
 import { Header } from '../../components/Header';
 import { LoginForm } from '../../components/LoginForm';
-import { SignUpForm } from '../../components/SignUpForm/SignUpForm';
+import { SignUpForm } from '../../components/SignUpForm';
 import { Footer } from '../../components/Footer';
 import './LoginPage.css';
 
 export const LoginPage = () => {
+  const [formType, setFormType] = useState('login');
+
+  const toggleModal = () => {
+    setFormType(formType === 'login' ? 'signup' : 'login');
+  };
+
   return (
     <>
       <Head title="Login" description="Descrição da página Login" />
@@ -13,8 +20,19 @@ export const LoginPage = () => {
       <Header />
 
       <main className="login-container">
-        <LoginForm />
-        <SignUpForm />
+        {formType === 'login' ? (
+          <LoginForm />
+        ) : (
+          <div className="signup-form">
+            <h2 className="default-title">Crie a sua conta</h2>
+
+            <SignUpForm />
+          </div>
+        )}
+
+        <button className="btn-signup-form" onClick={toggleModal}>
+          {formType === 'login' ? 'Criar conta' : 'Já tem uma conta?'}
+        </button>
       </main>
 
       <Footer />
