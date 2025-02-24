06803b3903765f3f9e955e9de5372e851e13c747
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-24 13:36:39 -0300

    pequeno ajuste

--- src/pages/RegisterFormPage/RegisterFormPage.jsx
+++ src/pages/RegisterFormPage/RegisterFormPage.jsx
@@ -1,11 +1,14 @@
+import { Head } from '../../components/Head';
 import { Header } from '../../components/Header';
+import { SignUpForm } from '../../components/SignUpForm';
 import { Footer } from '../../components/Footer';
 import './RegisterFormPage.css';
-import { SignUpForm } from '../../components/SignUpForm';
 
 export const RegisterFormPage = () => {
   return (
     <>
+      <Head title="Register" description="Descrição da página Register" />
+
       <Header />
       <SignUpForm />
       <Footer />
