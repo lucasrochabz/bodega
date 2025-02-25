352f3964ff1629031e0ab5359bf9204c98967a8a
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-25 15:18:36 -0300

    pequeno ajuste

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -14,50 +14,46 @@ export const OrderDetails = ({ order }) => {
           <p>Pedido realizado em: {formattedDate(order.created_at)}</p>
         </div>
 
-        <div className="order-store">
-          <p>Vendido e entregue por: Bodega</p>
-        </div>
+        <p className="order-store">Vendido e entregue por: Bodega</p>
 
-        <div className="order-details-body">
-          <div className="resume">
-            <img src={imagePath} alt={order.name} />
-            <ul className="resume-list">
-              <li>
-                <h2>{order.name}</h2>
-              </li>
-              <li>
-                <p>Valor total: {formattedPriceToBRL(order.price)}</p>
-              </li>
-              <li>
-                <p>{order.status}</p>
-              </li>
-            </ul>
-          </div>
-
-          <ul className="delivery">
+        <div className="resume">
+          <img src={imagePath} alt={order.name} />
+          <ul className="resume-list">
             <li>
-              <h3>Informações de entrega</h3>
+              <h2>{order.name}</h2>
             </li>
             <li>
-              Endereço: <span>{order.street}</span>
+              <p>Valor total: {formattedPriceToBRL(order.price)}</p>
             </li>
             <li>
-              Número: <span>{order.number}</span>
-            </li>
-            <li>
-              Bairro: <span>{order.neighborhood}</span>
-            </li>
-            <li>
-              Cidade:{' '}
-              <span>
-                {order.city}, {order.state}
-              </span>
-            </li>
-            <li>
-              CEP: <span>{order.zip_code}</span>
+              <p>{order.status}</p>
             </li>
           </ul>
         </div>
+
+        <ul className="delivery">
+          <li>
+            <h3>Informações de entrega</h3>
+          </li>
+          <li>
+            Endereço: <span>{order.street}</span>
+          </li>
+          <li>
+            Número: <span>{order.number}</span>
+          </li>
+          <li>
+            Bairro: <span>{order.neighborhood}</span>
+          </li>
+          <li>
+            Cidade:{' '}
+            <span>
+              {order.city}, {order.state}
+            </span>
+          </li>
+          <li>
+            CEP: <span>{order.zip_code}</span>
+          </li>
+        </ul>
       </div>
     </>
   );
