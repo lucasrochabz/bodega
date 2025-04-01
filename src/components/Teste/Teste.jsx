d6f772e1171f5d155c2b362ed2a681ce166e9061
lucasrochabz <lucasbezerrar@gmail.com>
2025-04-01 00:34:50 -0300

    add component useMedia

--- src/components/Teste/Teste.jsx
+++ src/components/Teste/Teste.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react';
-import './Teste.css';
+import { useMedia } from '../../hooks';
 import { Button } from '../Button';
+import './Teste.css';
 
 export const Teste = () => {
-  const [mobile, setMobile] = useState(null);
+  const mobile = useMedia('(max-width: 800px)');
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const handleMobileMenu = () => {
+    console.log('clicou no menu');
+    setMobileMenu((prevMobileMenu) => !prevMobileMenu);
+  };
 
   return (
-    <div>
-      <h2>Componente de Teste</h2>
-      <nav className="nav-teste">
-        <Button type="primary">X {mobile && 'Button 1'}</Button>
-        <Button type="primary">X {mobile && 'Button 2'}</Button>
-        <Button type="primary">X {mobile && 'Button 3'}</Button>
-        <Button type="primary">X {mobile && 'Button 4'}</Button>
-      </nav>
-    </div>
+    <section>
+      <h2>Sejá bem vindo</h2>
+      {mobile && (
+        <Button type="secondary" onClick={handleMobileMenu}>
+          Menu
+        </Button>
+      )}
+
+      {mobileMenu && (
+        <nav className="nav-teste">
+          <Button type="primary">{mobile ? 'Button 1' : 'Teste 1'}</Button>
+          <Button type="primary">{mobile ? 'Button 2' : 'Teste 2'}</Button>
+          <Button type="primary">{mobile ? 'Button 3' : 'Teste 3'}</Button>
+          <Button type="primary">{mobile ? 'Button 4' : 'Teste 4'}</Button>
+        </nav>
+      )}
+    </section>
   );
 };
