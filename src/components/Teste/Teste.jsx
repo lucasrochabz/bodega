a4b42eb24f538e70892d93698f6bc025375dd2b0
lucasrochabz <lucasbezerrar@gmail.com>
2025-04-01 00:40:46 -0300

    pequeno ajuste

--- src/components/Teste/Teste.jsx
+++ src/components/Teste/Teste.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useMedia } from '../../hooks';
 import { Button } from '../Button';
 import './Teste.css';
@@ -21,12 +22,12 @@ export const Teste = () => {
         </Button>
       )}
 
-      {mobileMenu && (
+      {mobile && mobileMenu && (
         <nav className="nav-teste">
+          <Link to="/">Home</Link>
           <Button type="primary">{mobile ? 'Button 1' : 'Teste 1'}</Button>
           <Button type="primary">{mobile ? 'Button 2' : 'Teste 2'}</Button>
           <Button type="primary">{mobile ? 'Button 3' : 'Teste 3'}</Button>
-          <Button type="primary">{mobile ? 'Button 4' : 'Teste 4'}</Button>
         </nav>
       )}
     </section>
