7cea08df80c8ed776ce9bae077c3da1225e01217
lucasrochabz <lucasbezerrar@gmail.com>
2025-04-03 17:35:47 -0300

    ajuste ao menu de navegação

--- src/components/Teste/Teste.jsx
+++ src/components/Teste/Teste.jsx
@@ -1,35 +1,5 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMedia } from '../../hooks';
-import { Button } from '../Button';
 import './Teste.css';
 
 export const Teste = () => {
-  const mobile = useMedia('(max-width: 800px)');
-  const [mobileMenu, setMobileMenu] = useState(false);
-
-  const handleMobileMenu = () => {
-    console.log('clicou no menu');
-    setMobileMenu((prevMobileMenu) => !prevMobileMenu);
-  };
-
-  return (
-    <section className="teste-container">
-      <h2>Sejá bem vindo</h2>
-      {mobile && (
-        <button className="btn-menu-mobile" onClick={handleMobileMenu}>
-          Menu
-        </button>
-      )}
-
-      {mobile && mobileMenu && (
-        <nav className="nav-teste">
-          <Link to="/">Home</Link>
-          <Button type="primary">{mobile ? 'Button 1' : 'Teste 1'}</Button>
-          <Button type="primary">{mobile ? 'Button 2' : 'Teste 2'}</Button>
-          <Button type="primary">{mobile ? 'Button 3' : 'Teste 3'}</Button>
-        </nav>
-      )}
-    </section>
-  );
+  return <h2>Sejá bem vindo</h2>;
 };
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import { useMedia } from '../../hooks';
 import { Head } from '../../components/Head';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Footer } from '../../components/Footer';
+import { Button } from '../../components/Button';
 import './UserPage.css';
 
 export const UserPage = () => {
-  const [title, setTitle] = useState('');
   const location = useLocation();
+  const mobile = useMedia('(max-width: 800px)');
+
+  const [title, setTitle] = useState('');
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const handleMobileMenu = () => {
+    console.log('clicou no menu');
+    setMobileMenu((prevMobileMenu) => !prevMobileMenu);
+  };
 
   useEffect(() => {
     const { pathname } = location;
@@ -37,7 +47,29 @@ export const UserPage = () => {
       <main className="user-page-layout">
         <Sidebar />
         <article className="user-page">
-          <h1>{title}</h1>
+          <section className="teste-container">
+            <h1>{title}</h1>
+            {mobile && (
+              <button className="btn-menu-mobile" onClick={handleMobileMenu}>
+                Menu
+              </button>
+            )}
+
+            {mobile && mobileMenu && (
+              <nav className="nav-teste">
+                <Link to="/">Home</Link>
+                <Button type="primary">
+                  {mobile ? 'Button 1' : 'Teste 1'}
+                </Button>
+                <Button type="primary">
+                  {mobile ? 'Button 2' : 'Teste 2'}
+                </Button>
+                <Button type="primary">
+                  {mobile ? 'Button 3' : 'Teste 3'}
+                </Button>
+              </nav>
+            )}
+          </section>
           <Outlet />
         </article>
       </main>
