2ffe3712eceef4bbad1a4ecd57eb5748cbad0912
lucasrochabz <lucasbezerrar@gmail.com>
2025-10-22 16:12:17 -0300

    add module.css ao Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,21 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import { ROUTES } from '../../routes/paths';
-import './Header.css';
+import styles from './Header.module.css';
 
 const Header = () => {
   const { data } = useContext(UserContext);
 
   return (
-    <header className="header-bg">
-      <nav className="header">
+    <header className={styles.bg}>
+      <nav className={styles.header}>
         <Link to={ROUTES.HOME} aria-label="Bodega - Home">
-          <span className="logo">Bodega</span>
+          <span className={styles.logo}>Bodega</span>
         </Link>
-        <Link to={data ? ROUTES.ACCOUNT : ROUTES.LOGIN} className="btn-header">
+        <Link
+          to={data ? ROUTES.ACCOUNT : ROUTES.LOGIN}
+          className={styles.btnHeader}
+        >
           {data ? `Olá, ${data.first_name}` : 'Entre ou cadastre-se'}
         </Link>
       </nav>
