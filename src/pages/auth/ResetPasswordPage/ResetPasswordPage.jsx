b01d1ec8b1f14cc9d740df287bcd1c610b87600d
lucasrochabz <lucasbezerrar@gmail.com>
2026-03-16 20:02:54 -0300

    add ResetPasswordForm

--- src/pages/auth/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/auth/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,66 +1,21 @@
-import { useState } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
-import { ROUTES } from '../../../paths';
-import { useResetPassword } from '@/hooks/auth';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { ROUTES } from '@/paths';
 import { AuthLayout } from '@/components/layout/AuthLayout';
-import { Dialog } from '../../../components/ui/Dialog';
-import { Button } from '../../../components/ui/Button';
-import styles from './ResetPasswordPage.module.css';
+import { ResetPasswordForm } from '@/components/forms/ResetPasswordForm';
 
 const ResetPasswordPage = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+
   const token = searchParams.get('token');
 
-  const [newPassword, setNewPassword] = useState('');
-  const [showModal, setShowModal] = useState(false);
-
-  const { sendNewPassword, isLoading, error } = useResetPassword();
-  const buttonLabel = isLoading ? 'Redefinindo...' : 'Redefinir senha';
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (!token) return alert('Token inválido');
-
-    const response = await sendNewPassword({ token, newPassword });
-    if (response) return setShowModal(true);
+  const handleSuccess = () => {
+    navigate(ROUTES.auth.login);
   };
 
   return (
     <AuthLayout page="reset">
-      {showModal && (
-        <Dialog
-          show={showModal}
-          onClose={() => {
-            setShowModal(false);
-            navigate(ROUTES.auth.login);
-          }}
-        >
-          <h2>Senha Redefinida</h2>
-          <p>Você já pode acessar a plataforma com a nova senha.</p>
-        </Dialog>
-      )}
-
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <label htmlFor="newPassword" className="label">
-          Nova senha
-        </label>
-
-        <input
-          type="password"
-          name="newPassword"
-          id="newPassword"
-          onChange={(e) => setNewPassword(e.target.value)}
-          value={newPassword}
-          required
-          className="input"
-        />
-
-        {error && <p>{error}</p>}
-
-        <Button disabled={isLoading}>{buttonLabel}</Button>
-      </form>
+      <ResetPasswordForm token={token} onSuccess={handleSuccess} />
     </AuthLayout>
   );
 };
