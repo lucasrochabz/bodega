c2daa4657bb3d812948005566a2f167c9504943d
lucasrochabz <lucasbezerrar@gmail.com>
2026-03-17 15:48:31 -0300

    separando responsabilidades do ForgotPasswordForm

--- src/pages/auth/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ src/pages/auth/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -1,10 +1,22 @@
 import { AuthLayout } from '@/components/layout/AuthLayout';
+import { useForgotPassword } from '@/hooks/auth';
 import { ForgotPasswordForm } from '@/components/forms/ForgotPasswordForm';
 
 const ForgotPasswordPage = () => {
+  const { sendEmail, isLoading } = useForgotPassword();
+
+  const handleSubmit = async (values) => {
+    const response = await sendEmail(values.email);
+
+    if (response) {
+      alert('Email enviado.');
+      window.location.href = response;
+    }
+  };
+
   return (
     <AuthLayout page="forgot">
-      <ForgotPasswordForm />
+      <ForgotPasswordForm onSubmit={handleSubmit} isLoading={isLoading} />
     </AuthLayout>
   );
 };
